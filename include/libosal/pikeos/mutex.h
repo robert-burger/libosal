7c33c7ef7cdc64eaf5173a17c6ababfcb19a916f
Robert Burger <robert.burger@dlr.de>
2022-09-22 08:24:24 +0200

    add: pikeos mutex header

---
+++ include/libosal/pikeos/mutex.h
@@ -0,0 +1,40 @@
+/**
+ * \file pikeos/mutex.h
+ *
+ * \author Robert Burger <robert.burger@dlr.de>
+ *
+ * \date 07 Aug 2022
+ *
+ * \brief OSAL mutex header.
+ *
+ * OSAL mutex include header.
+ */
+
+/*
+ * This file is part of libosal.
+ *
+ * libosal is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libosal is distributed in the hope that 
+ * it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libosal. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef LIBOSAL_PIKEOS_MUTEX__H
+#define LIBOSAL_PIKEOS_MUTEX__H
+
+#include <p4ext_threads.h>
+
+typedef struct osal_mutex {
+    P4_mutex_t pikeos_mtx;
+} osal_mutex_t;
+
+#endif /* LIBOSAL_PIKEOS_MUTEX__H */
+
