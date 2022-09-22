d00969cc5d2ae03a828c7c2a3b6b16d5a1d474de
Robert Burger <robert.burger@dlr.de>
2022-09-22 08:28:36 +0200

    add: missing pikeos semaphore header

---
+++ include/libosal/pikeos/semaphore.h
@@ -0,0 +1,40 @@
+/**
+ * \file pikeos/semaphore.h
+ *
+ * \author Robert Burger <robert.burger@dlr.de>
+ *
+ * \date 07 Aug 2022
+ *
+ * \brief OSAL semaphore header.
+ *
+ * OSAL semaphore include header.
+ */
+
+/*
+ * This file is part of libosal.
+ *
+ * libosal is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libosal is distributed in the hope that 
+ * it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libosal. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef LIBOSAL_PIKEOS_SEMAPHORE__H
+#define LIBOSAL_PIKEOS_SEMAPHORE__H
+
+#include <p4ext_threads.h>
+
+typedef struct osal_semaphore {
+    P4_sem_t pikeos_sem;
+} osal_semaphore_t;
+
+#endif /* LIBOSAL_PIKEOS_SEMAPHORE__H */
+
--- include/libosal/semaphore.h
+++ include/libosal/semaphore.h
@@ -42,6 +42,10 @@
 #include <libosal/vxworks/semaphore.h>
 #endif
 
+#ifdef LIBOSAL_BUILD_PIKEOS
+#include <libosal/pikeos/semaphore.h>
+#endif
+
 #define OSAL_SEMAPHORE_ATTR__PROCESS_SHARED         0x00000020u
 
 typedef osal_uint32_t osal_semaphore_attr_t;
