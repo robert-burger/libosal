c96a1c9707d6d58d6f07006dac7eb1bddaff6b68
Robert Burger <robert.burger@dlr.de>
2023-03-29 06:43:22 +0200

    add vxworks shm header

---
+++ include/libosal/vxworks/shm.h
@@ -0,0 +1,39 @@
+/**
+ * \file vxworks/shm.h
+ *
+ * \author Robert Burger <robert.burger@dlr.de>
+ *
+ * \date 07 Aug 2022
+ *
+ * \brief OSAL shm header.
+ *
+ * OSAL shm include header.
+ */
+
+/*
+ * This file is part of libosal.
+ *
+ * libosal is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libosal is distributed in the hope that 
+ * it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libosal. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef LIBOSAL_VXWORKS_SHM__H
+#define LIBOSAL_VXWORKS_SHM__H
+
+typedef struct osal_shm {
+    int fd;
+    osal_size_t size;
+} osal_shm_t;
+
+#endif /* LIBOSAL_VXWORKS_SHM__H */
+
