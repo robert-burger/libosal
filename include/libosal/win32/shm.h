a1351efaf7b530ecc42a79a04bfd179f2bd607b6
Robert Burger <robert.burger@dlr.de>
2024-02-29 13:13:44 +0100

    add: win32 shm header

---
+++ include/libosal/win32/shm.h
@@ -0,0 +1,45 @@
+/**
+ * \file win32/shm.h
+ *
+ * \author Robert Burger <robert.burger@dlr.de>
+ *
+ * \date 29 Feb 2024
+ *
+ * \brief OSAL shm header.
+ *
+ * OSAL shm include header.
+ */
+
+/*
+ * This file is part of libosal.
+ *
+ * libosal is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * libosal is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libosal; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef LIBOSAL_WIN32_SHM__H
+#define LIBOSAL_WIN32_SHM__H
+
+#include "windows.h"
+
+typedef struct osal_shm {
+    HANDLE map_file;
+    LPCTSTR buf;
+
+    osal_size_t size;
+} osal_shm_t;
+
+#endif /* LIBOSAL_WIN32_SHM__H */
+
+
