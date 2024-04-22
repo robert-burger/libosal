43de032c0376fcc2b5911b51e815ed3a5748324c
Johannes Nix <Johannes.Nix@dlr.de>
2024-04-22 15:57:48 +0200

    test: add test for non-broadcast condition

--- tests/posix/test_condvar.cc
+++ tests/posix/test_condvar.cc
@@ -281,6 +281,244 @@ TEST(Condvar, ParallelMasked) {
   }
 }
 } // namespace condvar_multithread
+/*
+ This test is very similar to the multithread test,
+ but with the difference that each thread
+ does not get a broadcast signaling
+ but a single one.
+ */
+namespace condvar_single {
+const int LOOPCOUNT3 = 50000;
+const int NTHREADS = 20;
+const int NO_THREAD = -1;
+// minimum number of free threads before
+// the test proceeds to select a free thread
+// (this is introduced in order to incrase
+// randomness).
+const int MIN_FREE_THREADS_PROCEED = 2;
+
+/* Note: differently from some other tests, this struct
+   is shared between all threads! */
+
+typedef struct {
+  osal_condvar_t condvar[NTHREADS];
+  osal_mutex_t mutex[NTHREADS];
+
+  // begin of items protected by mutex
+  unsigned long event_count[NTHREADS];
+  // end of items protected by mutex
+  std::atomic<unsigned long> active_mask;
+
+  std::atomic<bool> stop;
+
+  // piping for clean termination
+  sem_t finished_sem;            // is signaled when a thread terminates
+  std::atomic<int> thread_count; // count of active threads
+
+} shared_t;
+
+typedef struct {
+  int thread_id;
+  shared_t *p_shared;
+
+} threadvar_t;
+
+void *test_condvar_separate(void *arg) {
+  assert(arg != nullptr);
+  // keep in mind that shared_objects is necessarily shared here,
+  // differently from some other test code.
+  threadvar_t thread_var = *((threadvar_t *)arg);
+  const int thread_id = thread_var.thread_id;
+  shared_t *p_shared_objects = thread_var.p_shared;
+
+  osal_retval_t orv;
+  int rv;
+  while (true) {
+    // note: if events are missed, this test will hang here
+    osal_mutex_lock(&p_shared_objects->mutex[thread_id]);
+    EXPECT_EQ(orv, OSAL_OK) << "error in receiver: osal_mutex_lock()";
+
+    orv = osal_condvar_wait(&p_shared_objects->condvar[thread_id],
+                            &p_shared_objects->mutex[thread_id]);
+    EXPECT_EQ(orv, OSAL_OK) << "error in receiver: osal_condvar_wait()";
+
+    if ((p_shared_objects->active_mask >> thread_id) & 1ul) {
+      p_shared_objects->event_count[thread_id]++;
+    }
+
+    // using p_shared_objects->stop is safe here because it
+    // is an atomic variable.
+    if (p_shared_objects->stop) {
+      if (verbose) {
+        printf("thread %i: stop signal received\n", thread_id);
+      }
+      orv = osal_mutex_unlock(&p_shared_objects->mutex[thread_id]);
+      EXPECT_EQ(orv, OSAL_OK) << "error in receiver: osal_mutex_unlock()";
+      break;
+    }
+    // increment event count, which is the "paylod" of the
+    // transaction
+
+    // register that thread is ready to receive new
+    // messages. This can be done outside of the lock
+    // since this uses an atomic variable.
+    ulong clear_mask = ~(1ul << thread_id);
+    std::atomic_fetch_and(&p_shared_objects->active_mask, clear_mask);
+
+    orv = osal_mutex_unlock(&p_shared_objects->mutex[thread_id]);
+    EXPECT_EQ(orv, OSAL_OK) << "error in receiver: mutex_unlock failed()";
+  }
+
+  // signal that thread is finished
+  if (verbose) {
+    printf("final count for thread %i: final =  %lu\n", thread_id,
+           p_shared_objects->event_count[thread_id]);
+  }
+
+  p_shared_objects->thread_count -= 1;
+  rv = sem_post(&p_shared_objects->finished_sem);
+  EXPECT_EQ(rv, 0) << "error when sending finished sem signal";
+
+  return nullptr;
+}
+
+// this just sends a number of post() events to multiple
+// receivers, which count the received events.
+// The difference to the other tests is that each
+// receiver holds an own lock and condition variable,
+// and is notified by post() instead of broadcast.
+
+TEST(Condvar, ParallelSingleNotification) {
+  shared_t shared_objects; /* shared data protected by
+                              condvar and mutex */
+
+  threadvar_t thread_vars[NTHREADS];
+
+  shared_objects.thread_count = 0;
+
+  shared_objects.stop = false;
+
+  assert(NTHREADS > 0);
+  assert(LOOPCOUNT3 > 0);
+  static_assert(NTHREADS <= CHAR_BIT * sizeof(shared_objects.active_mask));
+  osal_retval_t orv;
+  int rv;
+
+  pthread_t thread_ids[NTHREADS];
+  unsigned long event_count[NTHREADS];
+
+  shared_objects.active_mask = 0;
+
+  rv = sem_init(&shared_objects.finished_sem, 0, 0);
+  ASSERT_EQ(rv, 0) << "creating pthreads semaphore failed!";
+
+  // initialize count of active threads
+  shared_objects.thread_count = NTHREADS;
+  memset(event_count, 0, sizeof(event_count));
+  memset(shared_objects.event_count, 0, sizeof(shared_objects.event_count));
+  // start threads
+  for (int i = 0; i < NTHREADS; i++) {
+    orv = osal_condvar_init(&shared_objects.condvar[i], nullptr);
+    ASSERT_EQ(orv, OSAL_OK) << "osal_condvar_init() failed";
+
+    orv = osal_mutex_init(&shared_objects.mutex[i], nullptr);
+    ASSERT_EQ(orv, OSAL_OK) << "osal_mutex_init() failed";
+
+    thread_vars[i].p_shared = &shared_objects;
+    thread_vars[i].thread_id = i;
+
+    rv = pthread_create(/*thread*/ &(thread_ids[i]),
+                        /*pthread_attr*/ nullptr,
+                        /* start_routine */ test_condvar_separate,
+                        /* arg */ (void *)&thread_vars[i]);
+    //	      ASSERT_EQ(rv, 0) << "pthread_create() failed";
+  }
+
+  if (verbose) {
+    printf("parallel sender: start OK\n");
+  }
+
+  srand(1);
+  for (int i = 0; i < LOOPCOUNT3; i++) {
+    // pick randomly some idle thread
+    ulong free_mask = get_free_mask(&shared_objects.active_mask,
+                                    MIN_FREE_THREADS_PROCEED, NTHREADS);
+    // pick randomly some non-busy thread
+    int selected_thread = pick_random_from_bits(free_mask);
+    // register that thread as busy by flipping its bit
+    orv = osal_mutex_lock(&shared_objects.mutex[selected_thread]);
+    ASSERT_EQ(orv, OSAL_OK) << "osal_mutex_lock() failed";
+
+    std::atomic_fetch_or(&shared_objects.active_mask, (1ul << selected_thread));
+    // here, send a single notification to that thread
+
+    orv = osal_condvar_signal(&shared_objects.condvar[selected_thread]);
+    ASSERT_EQ(orv, OSAL_OK) << "osal_condvar_broadcast() failed";
+
+    // track thread-local count of sent messages for that thread
+    event_count[selected_thread]++;
+
+    orv = osal_mutex_unlock(&shared_objects.mutex[selected_thread]);
+    ASSERT_EQ(orv, OSAL_OK) << "osal_mutex_unlock() failed";
+  }
+
+  // instruct threads to stop
+  for (int i = 0; i < NTHREADS; i++) {
+    orv = osal_mutex_lock(&shared_objects.mutex[i]);
+    ASSERT_EQ(orv, OSAL_OK) << "stopping: osal_mutex_lock() failed";
+  }
+  shared_objects.stop = true;
+  for (int i = 0; i < NTHREADS; i++) {
+    orv = osal_condvar_broadcast(&shared_objects.condvar[i]);
+    ASSERT_EQ(orv, OSAL_OK) << "stopping: osal_condvar_broadcast() failed";
+    orv = osal_mutex_unlock(&shared_objects.mutex[i]);
+    ASSERT_EQ(orv, OSAL_OK) << "stopping: osal_mutex_unlock() failed";
+  }
+
+  struct timespec max_time = {time(nullptr) + 10, 0};
+
+  while (shared_objects.thread_count > 0) {
+    rv = sem_timedwait(&shared_objects.finished_sem, &max_time);
+    EXPECT_EQ(rv, 0) << "wait for termination failed";
+    if (rv) {
+      printf("wait for termination of threads timed out\n");
+      break;
+    }
+  }
+  if (verbose) {
+    printf("parallel sender: joining\n");
+  }
+  for (int i = 0; i < NTHREADS; i++) {
+    rv = pthread_join(/*thread*/ thread_ids[i],
+                      /*retval*/ nullptr);
+    EXPECT_EQ(rv, 0) << "pthread_join() failed";
+  }
+
+  rv = sem_destroy(&shared_objects.finished_sem);
+  EXPECT_EQ(rv, 0) << "could not destroy mutex";
+
+  for (int i = 0; i < NTHREADS; i++) {
+    orv = osal_condvar_destroy(&shared_objects.condvar[i]);
+    EXPECT_EQ(orv, OSAL_OK) << "osal_condvar_destroy() failed";
+
+    orv = osal_mutex_destroy(&shared_objects.mutex[i]);
+    ASSERT_EQ(orv, OSAL_OK) << "osal_mutex_destroy() failed";
+  }
+
+  // checking resulting counts
+
+  if (verbose) {
+    for (int i = 0; i < NTHREADS; i++) {
+      printf("count for thread %i: local = %lu, thread = %lu\n", i,
+             event_count[i], shared_objects.event_count[i]);
+    }
+  }
+  for (int i = 0; i < NTHREADS; i++) {
+    EXPECT_EQ(event_count[i], shared_objects.event_count[i])
+        << "the count of events does not match";
+  }
+}
+} // namespace condvar_single
 
 namespace condvar_timedwait {
 const int LOOPCOUNT2 = 50000;
