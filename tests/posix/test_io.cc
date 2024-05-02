a2b47921f14f3a9566ce303454a35124896e2643
Johannes Nix <Johannes.Nix@dlr.de>
2024-05-02 19:48:21 +0200

    test: improve coverage for postic/io.c

--- tests/posix/test_io.cc
+++ tests/posix/test_io.cc
@@ -15,6 +15,9 @@ TEST(IO, PrintF) {
 
   orv = osal_printf("this is %i\n", 100);
 
+#pragma message                                                                \
+    "FIXME: return value of osal_printf() does not return number of bytes"
+
   EXPECT_EQ(orv, 0) << " osal_printf(..., 100) did not return zero";
 }
 TEST(IO, PutS) {
@@ -22,6 +25,22 @@ TEST(IO, PutS) {
   osal_retval_t orv = osal_puts("abc");
 
   EXPECT_EQ(orv, 0) << " osal_puts() did not return zero";
+}
+
+osal_int32_t test_func(const char *format, const char *args, ...) {
+
+  va_list ap;
+  va_start(ap, args);
+  osal_int32_t orv = osal_vfprintf(stdout, format, ap);
+  va_end(ap);
+
+  return orv;
+}
+
+TEST(IO, vfprintf) {
+  osal_int32_t orv = test_func("test: %s, %s\n", "argument1", "argument2");
+
+  EXPECT_EQ(orv, 31) << " osal_vfprintf() did not return zero";
 }
 
 } // namespace test_io
