9bbd66c41d48ad1cb111733b3af96cede48f5524
Johannes Nix <Johannes.Nix@dlr.de>
2024-06-03 21:24:15 +0200

    test: priority inheritance parameters

--- tests/posix/test_mutex.cc
+++ tests/posix/test_mutex.cc
@@ -292,6 +292,49 @@ TEST(MutexSane, OwnerDead) {
   EXPECT_EQ(orv, 0) << "Could not destroy mutex";
 }
 
+TEST(MutexSane, InheritPar) {
+  osal_mutex_t my_mutex;
+  osal_mutex_attr_t attr;
+  osal_retval_t orv = {};
+
+  attr = OSAL_MUTEX_ATTR__PROTOCOL__INHERIT;
+
+  orv = osal_mutex_init(&my_mutex, &attr);
+  ASSERT_EQ(orv, 0) << "Could not initialize mutex";
+
+  orv = osal_mutex_lock(&my_mutex);
+  EXPECT_EQ(orv, 0) << "Could not lock priority-inheritance mutex";
+
+  orv = osal_mutex_unlock(&my_mutex);
+  EXPECT_EQ(orv, 0) << "Could not unlock  priority-inheritance mutex";
+
+  orv = osal_mutex_destroy(&my_mutex);
+  EXPECT_EQ(orv, 0) << "Could not destroy  priority-inheritance mutex";
+}
+
+TEST(MutexSane, ProtectPar) {
+  osal_mutex_t my_mutex;
+  osal_mutex_attr_t attr;
+  osal_retval_t orv = {};
+
+  attr = OSAL_MUTEX_ATTR__PROTOCOL__PROTECT |
+         (1u << OSAL_MUTEX_ATTR__PRIOCEILING__SHIFT);
+
+  orv = osal_mutex_init(&my_mutex, &attr);
+  ASSERT_EQ(orv, 0) << "Could not initialize mutex";
+
+  orv = osal_mutex_lock(&my_mutex);
+  EXPECT_EQ(orv, OSAL_ERR_INVALID_PARAM) << "Could not lock prio-protect mutex";
+
+  if (orv == 0) {
+    orv = osal_mutex_unlock(&my_mutex);
+    EXPECT_EQ(orv, 0) << "Could not unlock prio-protect mutex";
+  }
+
+  orv = osal_mutex_destroy(&my_mutex);
+  EXPECT_EQ(orv, 0) << "Could not destroy mutex";
+}
+
 } // namespace test_mutex
 
 int main(int argc, char **argv) {
